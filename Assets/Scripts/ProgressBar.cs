ff1d07856944518aa246cdd58b177095f234c619
Tim Butler <tim.bax@web.de>
2020-09-20 11:47:12 +0200

    PromilleBar halb fertig

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+
+    private Slider slider;
+    private float targetValue = 0;
+    public float fillSpeed = 0.5f;
+
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Progress(1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slider.value < targetValue)
+            slider.value += fillSpeed *Time.deltaTime;
+        
+    }
+
+    public void Progress(float newProgress)
+    {
+        targetValue = slider.value + newProgress;
+    }
+
+}
